f1851cb0ceb761e082e0810ef1471f4b7db501ef
King-Success <code4kings@gmail.com>
2018-07-11 21:44:31 +0100

    implemented datatables search for subjects

---
+++ app/Http/Controllers/SubjectController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubjectController extends Controller
+{
+
+    /**
+    * Process datatables ajax request.
+    *
+    * @return \Illuminate\Http\JsonResponse
+    */
+    public function subjectDatatable() {
+
+        $rawSubjects = Subject::all();
+        $subjects = array();
+        foreach($rawSubjects as $rawSubject){
+            $subject = array();
+
+            $id = $rawSubject['id'];
+            $subject = $rawSubject['name'];
+            $creation = $rawSubject['created_at'];
+
+            // inject records into subject array
+            $subject['id'] = $id;
+            $subject['name'] = $subject;
+            $subject['created_at'] = $creation;
+            //push subject array into subjects array
+            array_push($subjects, $subject);
+
+        }
+        return Datatables::of($subjects)
+                ->editColumn('created_at', function($data){
+                    return date('d F Y', strtotime($data['created_at']));
+                })
+                ->addColumn('actions', function($data){
+                     return "<a class='btn btn-primary' href='/subject/" . $data['id'] . "/scoresheet/1/1/edit'>Edit</a> <a class='btn btn-danger' href='/subject/" . $data['id'] . "/delete'>Delete</a>";
+                })
+                ->rawColumns(['actions'])
+                ->make(true);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
