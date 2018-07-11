0813332f2cff2c37cf148dd5578db84ca18b4acf
King-Success <code4kings@gmail.com>
2018-07-11 19:45:46 +0100

    implemented the controller methods for staffController

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -6,6 +6,48 @@ use Illuminate\Http\Request;
 
 class StaffController extends Controller
 {
+
+    public function staffDatatable() {
+
+        $rawStaffs = Staff::all();
+        $staffs = array();
+        foreach($rawStaffs as $rawStaff){
+            $staff = array();
+
+            $id = $rawStaff['id'];
+            $firstName = $rawStaff['firstname'];
+            $lastName = $rawStaff['lastname'];
+            $otherName = $rawStaff['othername'];
+            $gender = $rawStaff['gender'];
+            $mobileNumber = $rawStaff['mobile_number'];
+            $DOB = $rawStaff['DOB'];
+            $nationality = $rawStaff['nationality'];
+            $creation = $rawStaff['created_at'];
+
+            // construct a fullname variable here
+            $fullName = $firstName. ' ' . $otherName . ' '. $lastName;
+            // inject records into staff array
+            $staff['id'] = $id;
+            $staff['fullname'] = $fullName;
+            $staff['gender'] = $gender;
+            $staff['mobile_number'] = $mobileNumber;
+            $staff['DOB'] = $DOB;
+            $staff['nationality'] = $nationality;
+            $staff['created_at'] = $creation;
+            //push staff array into staffs array
+            array_push($staffs, $staff);
+
+        }
+        return Datatables::of($staffs)
+                ->editColumn('created_at', function($data){
+                    return date('d F Y', strtotime($data['created_at']));
+                })
+                ->addColumn('actions', function($data){
+                     return "<a class='btn btn-primary' href='/staff/" . $data['id'] . "/scoresheet/1/1/edit'>Profile</a> <a class='btn btn-danger' href='/staff/" . $data['id'] . "/delete'>Delete</a>";
+                })
+                ->rawColumns(['actions'])
+                ->make(true);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +55,11 @@ class StaffController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+        if($user->isAdmin || $user->isStaff){
+            return view('staff.index');
+        }
+        return Redirect::to('/home');
     }
 
     /**
@@ -23,7 +69,7 @@ class StaffController extends Controller
      */
     public function create()
     {
-        //
+        return view('staff.create');
     }
 
     /**
@@ -34,7 +80,35 @@ class StaffController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = Auth::user();
+        if($user->isAdmin || $user->isStaff){
+            $rules = array(
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'othername' => 'required',
+            'gender' => 'required',
+            'DOB' => 'required',
+            'mobile_number' => 'required|numeric',
+            'nationality' => 'required',
+            'state' => 'required',
+            'LGA' => 'required',
+            'religion' => 'required',
+            'class_id' => 'required|integer',
+        );
+
+        $validator = Validator::make(Input::all(), $rules);
+
+        if ($validator->fails()) {
+            return Redirect::to('/staff/create')
+                ->with('status', 'Staff creation Failed')
+                ->withErrors($validator)
+                ->withInput(Input::all());
+        } else {
+            $staff = Staff::create(Input::all());
+            return Redirect::to('/staff')
+                ->with('status', 'Created Successfully');
+        }
+    }
     }
 
     /**
@@ -79,6 +153,16 @@ class StaffController extends Controller
      */
     public function destroy($id)
     {
-        //
+        if(Auth::user()->isAdmin){
+               $response = Staff::where('id', $id)->delete(); 
+               if(!$response) {
+                   return Redirect::to('/staff')
+                   ->with('status', 'Failed to delete');
+               }
+               return Redirect::to('/staff')
+                ->with('status', 'Deleted Successfully');
+            }
+            return Redirect::to('/staff')
+                ->with('status', 'Insufficient priviledge');
     }
 }
